3fe18ce6ace8581fda214ec8783c3b82858b9b51
azambembem <azamjan980713@gmail.com>
2024-09-05 15:54:28 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex w-full h-screen items-center justify-center">
+      <Button className="text-red-500">hello</Button>
+    </div>
+  );
+}
+
+export default App;
