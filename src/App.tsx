551100ab0c8ea32ac2d54989b129f032633fc8ae
azambembem <azamjan980713@gmail.com>
2024-09-10 01:02:56 +0900

    feat: statistics commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import BookSection from "./components/books-section";
 import Flayer from "./components/flayer";
 import MobileApp from "./components/mobile-app";
 import Navbar from "./components/navbar";
+import Statistics from "./components/statistics";
 
 function App() {
-	return (
-		<div>
-			<Toaster />
-			<Navbar />
-			<Flayer />
-			<BookSection />
-			<MobileApp />
-		</div>
-	);
+  return (
+    <div>
+      <Toaster />
+      <Navbar />
+      <Flayer />
+      <BookSection />
+      <MobileApp />
+      <Statistics />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/statistics/index.tsx
@@ -0,0 +1,47 @@
+const Statistics = () => {
+  return (
+    <div className="w-[90%] m-auto mt-[100px] flex gap-4">
+      <div className="bg-[#D658111A] flex-1 flex gap-2 items-center py-[28px] px-[24px] rounded-[8px]">
+        <div className="w-[72px] h-[72px] bg-[#fff] flex items-center justify-center rounded-full">
+          <img
+            className="w-[44px] h-[44px]"
+            src="/icons/book_more.svg"
+            alt="book_more"
+          />
+        </div>
+        <div>
+          <h3 className="font-semibold text-lg">30 000</h3>
+          <p>Kitob sotuvda mavjud</p>
+        </div>
+      </div>
+      <div className="bg-[#D658111A] flex-1 flex gap-2 items-center py-[28px] px-[24px] rounded-[8px]">
+        <div className="w-[72px] h-[72px] bg-[#fff] flex items-center justify-center rounded-full">
+          <img
+            className="w-[44px] h-[44px]"
+            src="/icons/book_more.svg"
+            alt="book_more"
+          />
+        </div>
+        <div>
+          <h3 className="font-semibold text-lg">30 000</h3>
+          <p>Kitob sotuvda mavjud</p>
+        </div>
+      </div>
+      <div className="bg-[#D658111A] flex-1 flex gap-2 items-center py-[28px] px-[24px] rounded-[8px]">
+        <div className="w-[72px] h-[72px] bg-[#fff] flex items-center justify-center rounded-full">
+          <img
+            className="w-[44px] h-[44px]"
+            src="/icons/book_more.svg"
+            alt="book_more"
+          />
+        </div>
+        <div>
+          <h3 className="font-semibold text-lg">30 000</h3>
+          <p>Kitob sotuvda mavjud</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
