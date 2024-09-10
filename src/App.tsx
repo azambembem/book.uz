e315cf25e4d9743b6cb49f90ce3fd0ee3740a881
azambembem <azamjan980713@gmail.com>
2024-09-11 00:09:05 +0900

    feat: last-pages-homepage commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 import { Toaster } from "@/components/ui/sonner";
 import BookSection from "./components/books-section";
 import Flayer from "./components/flayer";
+import LastPages from "./components/last-pages";
 import MobileApp from "./components/mobile-app";
 import MonthTopBooks from "./components/month-top-books/imdex";
 import Navbar from "./components/navbar";
 import News from "./components/news";
 import Statistics from "./components/statistics";
 import Week from "./components/week";
-import LastPages from "./components/last-pages";
 
 function App() {
-  return (
-    <div>
-      <Toaster />
-      <Navbar />
-      <Flayer />
-      <BookSection />
-      <Week />
-      <MonthTopBooks />
-      <MobileApp />
-      <Statistics />
-      <News />
-      <LastPages />
-    </div>
-  );
+	return (
+		<div>
+			<Toaster />
+			<Navbar />
+			<Flayer />
+			<BookSection />
+			<Week />
+			<MonthTopBooks />
+			<MobileApp />
+			<Statistics />
+			<News />
+			<LastPages />
+		</div>
+	);
 }
 
 export default App;
