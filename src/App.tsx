272fd0bb153d4e9939477807fbb6c4ecb1bb2766
azambembem <azamjan980713@gmail.com>
2024-09-10 20:08:24 +0900

    feat: month-top-books commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,23 @@ import MobileApp from "./components/mobile-app";
 import Navbar from "./components/navbar";
 import News from "./components/news";
 import Statistics from "./components/statistics";
+import Week from "./components/week";
+import MonthTopBooks from "./components/month-top-books/imdex";
 
 function App() {
-	return (
-		<div>
-			<Toaster />
-			<Navbar />
-			<Flayer />
-			<BookSection />
-			<MobileApp />
-			<Statistics />
-			<News />
-		</div>
-	);
+  return (
+    <div>
+      <Toaster />
+      <Navbar />
+      <Flayer />
+      <BookSection />
+      <Week />
+      <MonthTopBooks />
+      <MobileApp />
+      <Statistics />
+      <News />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/month-top-books/imdex.tsx
@@ -0,0 +1,14 @@
+import Bodyyy from "./customs/bodyyy";
+
+const MonthTopBooks = () => {
+  return (
+    <div className="w-full h-[554px] mt-[70px] bg-[#F0F0F0]">
+      <div className="flex ml-16">
+        <h3 className="text-2xl mt-8">Oyning top kitoblari</h3>
+      </div>
+      <Bodyyy />
+    </div>
+  );
+};
+
+export default MonthTopBooks;
