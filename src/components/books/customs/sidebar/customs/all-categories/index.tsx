1700b5fec8636a39b4fa8460f71fcde494081e69
azambembem <azamjan980713@gmail.com>
2024-09-30 23:46:16 +0900

    categories commit

--- src/components/books/customs/sidebar/customs/all-categories/index.tsx
+++ src/components/books/customs/sidebar/customs/all-categories/index.tsx
@@ -2,48 +2,68 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useSearchAppParams } from "@/hooks/useRedux/useSearchParams";
+import { useEffect, useState } from "react";
+import axios from "axios";
+// import { Skeleton } from "@/components/ui/skeleton";
 
 const tags = Array.from({ length: 50 }).map(
-	(_, i, a) => `v1.2.0-beta.${a.length - i}`,
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`
 );
 
 const AllCategories = () => {
-	const { setParams } = useSearchAppParams();
-	return (
-		<div className="p-4 bg-[#f6f6f6]">
-			<div className="flex items-center space-x-2 h-[52px]">
-				<Checkbox id="terms" />
-				<label
-					htmlFor="terms"
-					className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-				>
-					All Categories
-				</label>
-			</div>
-			<Input
-				onChange={(e) => setParams({ "category-search": e.target.value })}
-				placeholder="Qidirish"
-				type="Search"
-			/>
-			<ScrollArea className="h-72 rounded-md">
-				<div>
-					{tags.map((tag) => (
-						<div key={tag}>
-							<div className="flex items-center space-x-2 h-[48px]">
-								<Checkbox id={tag} />
-								<label
-									htmlFor={tag}
-									className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-								>
-									Badiiy Adabiyot
-								</label>
-							</div>
-						</div>
-					))}
-				</div>
-			</ScrollArea>
-		</div>
-	);
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const { data } = await axios({
+        url: "http://localhost:8080/api/category/new-age-library",
+        method: "GET"
+      });
+      setLoading(false);
+
+      setCategories(data.data);
+      console.log(data);
+    })();
+  }, []);
+
+  const { setParams } = useSearchAppParams();
+  return (
+    <div className="p-4 bg-[#f6f6f6]">
+      <div className="flex items-center space-x-2 h-[52px]">
+        <Checkbox id="terms" />
+        <label
+          htmlFor="terms"
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          All Categories
+        </label>
+      </div>
+      <Input
+        onChange={(e) => setParams({ "category-search": e.target.value })}
+        placeholder="Qidirish"
+        type="Search"
+      />
+      <ScrollArea className="h-72 rounded-md">
+        <div>
+          {tags.map((tag) => (
+            <div key={tag}>
+              <div className="flex items-center space-x-2 h-[48px]">
+                <Checkbox id={tag} />
+                <label
+                  htmlFor={tag}
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Badiiy Adabiyot
+                </label>
+              </div>
+            </div>
+          ))}
+        </div>
+      </ScrollArea>
+    </div>
+  );
 };
 
 export default AllCategories;
--- src/components/books/selected-book/customs/book-material/customs/slide/index.tsx
+++ src/components/books/selected-book/customs/book-material/customs/slide/index.tsx
@@ -1,37 +1,17 @@
 const Slide = () => {
   return (
     <div className="flex-1 flex gap-4 h-[543px] ">
-      <div className="w-[85px] grid-cols-1 grid-rows-5">
+      <div className="w-[85px]">
         <img
           className="w-full h-full object-cover"
-          src="https://th.bing.com/th/id/OIP.b8hKSSJLk1jRUOe911yl6gHaLH?w=186&h=279&c=7&r=0&o=5&dpr=1.9&pid=1.7"
-          alt=""
-        />
-        <img
-          className="w-full h-full object-cover"
-          src="https://th.bing.com/th/id/OIP.b8hKSSJLk1jRUOe911yl6gHaLH?w=186&h=279&c=7&r=0&o=5&dpr=1.9&pid=1.7"
-          alt=""
-        />
-        <img
-          className="w-full h-full object-cover"
-          src="https://th.bing.com/th/id/OIP.b8hKSSJLk1jRUOe911yl6gHaLH?w=186&h=279&c=7&r=0&o=5&dpr=1.9&pid=1.7"
-          alt=""
-        />
-        <img
-          className="w-full h-full object-cover"
-          src="https://th.bing.com/th/id/OIP.b8hKSSJLk1jRUOe911yl6gHaLH?w=186&h=279&c=7&r=0&o=5&dpr=1.9&pid=1.7"
-          alt=""
-        />
-        <img
-          className="w-full h-full object-cover"
-          src="https://th.bing.com/th/id/OIP.b8hKSSJLk1jRUOe911yl6gHaLH?w=186&h=279&c=7&r=0&o=5&dpr=1.9&pid=1.7"
-          alt=""
+          src="https://th.bing.com/th/id/OIP.6r5c5mC39DfewdphcY6GNQHaF7?w=186&h=149&c=7&r=0&o=5&dpr=1.9&pid=1.7"
+          alt="123"
         />
       </div>
       <img
         className="w-full h-full object-cover"
-        src="https://th.bing.com/th/id/OIP.b8hKSSJLk1jRUOe911yl6gHaLH?w=186&h=279&c=7&r=0&o=5&dpr=1.9&pid=1.7"
-        alt=""
+        src="https://th.bing.com/th/id/OIP.6r5c5mC39DfewdphcY6GNQHaF7?w=186&h=149&c=7&r=0&o=5&dpr=1.9&pid=1.7"
+        alt="123"
       />
     </div>
   );
